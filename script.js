4da6851a1c07f4ece424385bdf1d1314297056da
APG12 <30400126+APG12@users.noreply.github.com>
2017-08-14 20:56:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -217,3 +217,13 @@ annyang.addCallback('result', function(phrases) {
                                audio = zaglushki[Math.floor(Math.random()*zaglushki.length)];
             });
                     }
+
+
+function pickRandomProperty(obj) {
+    var result;
+    var count = 0;
+    for (var prop in obj)
+        if (Math.random() < 1/++count)
+           result = prop;
+    return result;
+}
