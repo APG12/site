d31f31c3d6b9a664ccc66bc04239279283f1a9b4
APG12 <30400126+APG12@users.noreply.github.com>
2017-08-14 19:46:51 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+
+	annyang.debug();
+annyang.setLanguage("ru");
+annyang.start();
+annyang.addCallback('soundstart', function() {
+  console.log('sound detected');
+});
+
+annyang.addCallback('result', function() {
+  console.log('sound stopped');
+});
+
+annyang.addCallback('result', function(phrases) {
+  console.log("I think the user said: ", phrases[0]);
+  console.log("But then again, it could be any of the following: ", phrases);
+});
+}
