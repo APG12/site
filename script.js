39fcfec507d3af976b8ab324cd89b14d06d5a2ae
APG12 <30400126+APG12@users.noreply.github.com>
2017-08-14 22:19:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -183,7 +183,7 @@ annyang.addCallback('result', function(phrases) {
                                  {
                                     if (fraz[fraz[maxflag].chain]){
                                     fraz[fraz[maxflag].chain].w += 0.1;
-                                    console.log([fraz[maxflag].chain + " " + fraz[fraz[maxflag].chain].w);
+                                    console.log(fraz[maxflag].chain + " " + fraz[fraz[maxflag].chain].w);
 
                                     }
                                  }
