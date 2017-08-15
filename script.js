fddbd4b1cd3de37d0c803901ebdbfe7adf479a02
APG12 <30400126+APG12@users.noreply.github.com>
2017-08-15 15:02:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -184,7 +184,7 @@ annyang.addCallback('result', function(phrases) {
                                 var maxflagb = maxflag;
                                 var sonu = Math.floor(Math.random()*fraz[maxflag].sound.length);
                                 audio = new Audio(fraz[maxflag].sound[sonu][0]);
-
+                               console.log(fraz[maxflag].sound.length + " записей звуковых");
                                 audio.play();        
                                if (fraz[maxflag].sound[sonu].length >1){
                                audio.addEventListener("ended", function(){
