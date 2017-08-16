fb2620481343f01824e9a9e2ea58e125d84a4364
APG12 <30400126+APG12@users.noreply.github.com>
2017-08-16 23:53:13 +0800

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,20 +1,19 @@
 window.onload = function() {
-    annyang.debug();
-    annyang.setLanguage("ru");
-    annyang.start({
-        continuous: false
-    });
-    annyang.addCallback('soundstart', function() {
-    });
-    annyang.addCallback('result', function(phrases) {
+        annyang.debug();
+        annyang.setLanguage("ru");
+        annyang.start({
+            continuous: false
+        });
+        annyang.addCallback('soundstart', function() {});
+        annyang.addCallback('result', function(phrases) {
 
-                var words = phrases[0].split(" ");
-                for (var j = 0; j < words.length; j++) {
-                words[i] = "хуе" + words[i].slice(3, words[i].length-1);
-            console.log(words[i]);
+            var words = phrases[0].split(" ");
+            for (var j = 0; j < words.length; j++) {
+                words[i] = "хуе" + words[i].slice(3, words[i].length - 1);
+                console.log(words[i]);
             };
 
-  var leftData = "<div class='message left' style='display:none;'><div class='message-text'>" + words +
+            var leftData = "<div class='message left' style='display:none;'><div class='message-text'>" + words +
                 "</div></div>";
             $(leftData).prependTo('#phone').fadeIn('slow', function() {
 
@@ -22,5 +21,6 @@ window.onload = function() {
                     "</div></div>";
                 $(rightData).prependTo('#phone').fadeIn('slow');
             });
- 
-});
+
+        });
+};
