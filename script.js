987e4663a472b03847a16d6cb89b26c4d0bcdf52
APG12 <30400126+APG12@users.noreply.github.com>
2017-09-01 01:00:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1085,9 +1085,30 @@ window.onload = function() {
             slova: ["любое слово"],
             sound: ['sounds/да пошел ты мудак обьеб.mp3'],
             text: ['Да пошел ты нахуй, мудак объебаный.'],
+            chain: ['fartmode'],
             w: 0.94,
             bw: 0.94
         },
+        
+        fartmode: {
+            slova: ["любое слово"],
+            sound: [
+                ['sounds/fart-01.mp3'],
+                ['sounds/fart-02.mp3'],
+                ['sounds/fart-03.mp3'],
+                ['sounds/fart-04.mp3'],
+                ['sounds/fart-05.mp3'],
+                ['sounds/fart-06.mp3'],
+                ['sounds/fart-07.mp3'],
+                ['sounds/fart-08.mp3'],
+                
+                   ],
+            text: ['ПЕРДЕЖНО-КАРАТЕЛЬНЫЙ РЕЖИМ АКТИВИРОВАН'],
+            chain: ['fartmode'],
+            w: 0.94,
+            bw: 0.94
+        },
+        
      
     };
 
