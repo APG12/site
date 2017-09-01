31a56a504f85941ec616b1fdda8250787df639a4
APG12 <30400126+APG12@users.noreply.github.com>
2017-09-01 20:38:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1123,25 +1123,7 @@ window.onload = function() {
         
         
         
-        fartmode: {
-            slova: ["любое слово"],
-            sound: [
-                ['sounds/fart-01.mp3'],
-                ['sounds/fart-02.mp3'],
-                ['sounds/fart-03.mp3'],
-                ['sounds/fart-04.mp3'],
-                ['sounds/fart-05.mp3'],
-                ['sounds/fart-06.mp3'],
-                ['sounds/fart-07.mp3'],
-                ['sounds/fart-08.mp3'],
-                
-                   ],
-            text: ['ПЕРДЕЖНО-КАРАТЕЛЬНЫЙ РЕЖИМ АКТИВИРОВАН'],
-            chain: ['fartmode'],
-            w: 0,
-            bw: 0
-        },
-        
+ 
      
     };
 
